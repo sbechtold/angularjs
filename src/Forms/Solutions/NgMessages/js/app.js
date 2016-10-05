accb543f6f306a43ec3c6307f07e731fead655c1
Joe Price <josephrprice@gmail.com>
2016-10-05 11:01:52 -0400

    Forms/Solutions/NgMessages

--- src/Forms/Solutions/NgMessages/js/app.js
+++ src/Forms/Solutions/NgMessages/js/app.js
@@ -1,4 +1,31 @@
-angular.module("productApp", ["ngRoute", "productApp.controllers"])
+angular.module("productApp", [
+    "ngRoute",
+    "productApp.controllers",
+    "ngSanitize",
+    //"ngMessages",
+    "ngResource",
+    "ui.router",
+    "ui.bootstrap",
+    "redbox.shared.utils",
+    "redbox.framework.directives",
+    //"redbox.framework.configuration",
+    "redbox.shared.services",
+    "adp.ng.language",
+    "adp.ng.ui.modal",
+    "adp.ng.meta",
+    "redbox.framework",
+    "redbox.framework.footer",
+    "redbox.framework.components",
+    "redbox.framework.meta",
+    "redbox.branding",
+    //"redbox.framework.templates",
+    //"redbox.framework.ext.templates",
+    "redbox.legal",
+    "redbox.shell"
+    //"ui.grid",
+    //"ui.grid.infiniteScroll",
+    //"benefits.redbox.core",
+])
     .config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
         $routeProvider
             .when("/", {
