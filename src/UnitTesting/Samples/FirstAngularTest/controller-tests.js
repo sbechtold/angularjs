34abf34da08834e8e31690f32745b3a7c57d4093
Stephen Bechtold <sbechtold@buildinmotion.com>
2016-10-11 13:49:14 -0400

    Added the controller add syntax to tests

--- src/UnitTesting/Samples/FirstAngularTest/controller-tests.js
+++ src/UnitTesting/Samples/FirstAngularTest/controller-tests.js
@@ -1,39 +1,76 @@
 describe("TestFixture: Controller", function () {
-
     var testScope, personToDelete, numberOfPeople;
+    describe("TestFixture: Controller", function () {
+        beforeEach(function () {
+            module('myEventHandlers');
+            inject(function ($controller, $rootScope) {
+                testScope = $rootScope.$new();
+                $controller('MainController', { $scope: testScope });
+                personToDelete = testScope.people[2];
+                numberOfPeople = testScope.people.length;
+            });
+        });
 
-    beforeEach( function () {
-        module( 'myEventHandlers' );
-        inject( function ( $controller, $rootScope ) {
-            testScope = $rootScope.$new();
-            $controller( 'MainController', { $scope : testScope } );
-            personToDelete = testScope.people[2];
-            numberOfPeople = testScope.people.length;
+        it("testCase: remove(one)", function () {
+            expect(testScope.people.length).toBe(numberOfPeople);
+
+            expect(testScope.people).toContain(personToDelete);
+            testScope.remove(personToDelete);
+
+            expect(testScope.people).not.toContain(personToDelete);
+            expect(testScope.people.length).toBe(numberOfPeople - 1);
+
+        });
+
+        it("testCase: Add a person", function () {
+            var person = { "Name": "Jacob", "Age": 31, "Gender": "Male", "Id": 8 };
+            expect(testScope.people.length).toBe(numberOfPeople);
+
+            testScope.addPerson = person;
+            expect(testScope.people).not.toContain(person);
+
+            testScope.add();
+            expect(testScope.addPerson).toEqual({});
+
+            expect(testScope.people).toContain(person);
+            expect(testScope.people.length).toBe(numberOfPeople + 1);
         });
     });
 
-    it("testCase: remove(one)", function () {
-        expect(testScope.people.length).toBe(numberOfPeople);
 
-        expect(testScope.people).toContain(personToDelete);
-        testScope.remove(personToDelete);
+    describe("TestFixture: Controller As", function () {
+        beforeEach(function () {
+            module('myEventHandlers');
+            inject(function ($controller, $rootScope) {
+                testScope = $controller('MainControllerAs');
+                personToDelete = testScope.people[2];
+                numberOfPeople = testScope.people.length;
+            });
+        });
 
-        expect(testScope.people).not.toContain(personToDelete);
-        expect(testScope.people.length).toBe(numberOfPeople - 1);
+        it("testCase: remove(one)", function () {
+            expect(testScope.people.length).toBe(numberOfPeople);
 
-    });
+            expect(testScope.people).toContain(personToDelete);
+            testScope.remove(personToDelete);
 
-    it("testCase: Add a person", function () {
-        var person = { "Name":"Jacob", "Age":31, "Gender":"Male", "Id":8 };
-        expect(testScope.people.length).toBe(numberOfPeople);
+            expect(testScope.people).not.toContain(personToDelete);
+            expect(testScope.people.length).toBe(numberOfPeople - 1);
 
-        testScope.addPerson = person;
-        expect(testScope.people).not.toContain(person);
+        });
 
-        testScope.add();
-        expect(testScope.addPerson).toEqual({});
+        it("testCase: Add a person", function () {
+            var person = { "Name": "Jacob", "Age": 31, "Gender": "Male", "Id": 8 };
+            expect(testScope.people.length).toBe(numberOfPeople);
 
-        expect(testScope.people).toContain(person);
-        expect(testScope.people.length).toBe(numberOfPeople + 1);
+            testScope.addPerson = person;
+            expect(testScope.people).not.toContain(person);
+
+            testScope.add();
+            expect(testScope.addPerson).toEqual({});
+
+            expect(testScope.people).toContain(person);
+            expect(testScope.people.length).toBe(numberOfPeople + 1);
+        });
     });
 });
--- src/UnitTesting/Samples/FirstAngularTest/controllers.js
+++ src/UnitTesting/Samples/FirstAngularTest/controllers.js
@@ -36,3 +36,42 @@ app.controller("MainController", ["$scope", function ($scope) {
      
    };
 }]);
+
+
+app.controller("MainControllerAs", [function () {
+    var vm = this;
+    vm.people = [
+        { "Name": "Bob", "Age": 23, "Gender": "Male", "Id": 1 },
+        { "Name": "Chuck", "Age": 31, "Gender": "Male", "Id": 2 },
+        { "Name": "John", "Age": 25, "Gender": "Male", "Id": 3 },
+        { "Name": "Kate", "Age": 42, "Gender": "Female", "Id": 4 },
+        { "Name": "Abigail", "Age": 32, "Gender": "Female", "Id": 5 },
+        { "Name": "Doris", "Age": 26, "Gender": "Female", "Id": 6 },
+        { "Name": "Jean", "Age": 37, "Gender": "Male", "Id": 7 }
+    ];
+
+    vm.addPerson = {}; // Set an empty object
+
+    vm.handleEvent = function (evt) {
+        $scope.lastEvent = evt.type;
+    };
+
+
+    vm.remove = function (person, evt) {
+        if (evt)
+            vm.handleEvent(evt);
+
+        var location = vm.people.indexOf(person);
+        if (location > -1)
+            vm.people.splice(location, 1);
+    };
+
+    vm.add = function (evt) {
+        if (evt)
+            vm.handleEvent(evt);
+
+        vm.people.push(vm.addPerson);
+        vm.addPerson = {};
+
+    };
+}]);
